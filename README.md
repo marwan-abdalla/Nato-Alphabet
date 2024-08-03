f2038eb23ede992ba6bb0a892ff8a31d6b2e0a85
Marwan Abdalla <marwan.maro201@gmail.com>
2024-08-03 03:12:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## NATO Phonetic Alphabet Converter
+
+### Project Overview
+
+This simple program converts user-input sentences into the NATO phonetic alphabet. Each letter of the sentence you type is translated into its corresponding NATO phonetic code. Very helpful when spelling out words on the phone!
+
+### Project Steps
+
+1. Read and Load NATO Alphabet Data
+2. Ask User for Input
+3. Convert Input to NATO Phonetic Alphabet
+4. Display the Result
+
+### File Overview
+
+- nato_converter.py - The main Python script that runs the converter.
+
+### Installation
+
+To use this program, please install the following locally:
+
+1. Python 3.8+
+2. Python Packages:
+   - Pandas
+
+### Contributing
+
+Contributions are welcome! If you have any suggestions for improvement, please create a pull request or open an issue.
+
+Enjoy using the NATO Phonetic Alphabet Converter!
---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+def nato_dictionary():
+    # Read the CSV file into a DataFrame
+    alphabet_data_frame = pd.read_csv("nato_phonetic_alphabet.csv")
+    # Create the dictionary from the DataFrame
+    nato_dict = {row["letter"]: row["code"] for index, row in alphabet_data_frame.iterrows()}
+    # Asks user to input a sentence
+    user_input = input("Please type a sentence: ").upper()
+    # Converts the input sentence into NATO phonetic alphabet words
+    nato_dict = [nato_dict[letter] for letter in user_input]
+
+    return nato_dict
+
+
+def main():
+
+    """
+    Main function to run the NATO phonetic alphabet conversion program.
+
+    """
+    continue_using = True
+
+    while continue_using:
+        user_decision = input("Would you like to input a sentence? Type 'Y' or 'N': ")
+        if user_decision.upper() == 'Y':
+            print(nato_dictionary())
+        elif user_decision.upper() == 'N':
+            continue_using = False
+        else:
+            print("Invalid input, Please Type 'Y' or 'N'.")
+
